9ff83028e83e1b9c36a4296c528800638a4209a3
Sujay Anjankar <Sujay.Anjankar.consultant@nielsen.com>
2016-12-28 10:43:44 +0530

    Added gitignore.

--- GFGExamples/src/com/examples/tree/Tree.java
+++ GFGExamples/src/com/examples/tree/Tree.java
@@ -4,33 +4,40 @@ public class Tree {
 	private Integer data;
 	private Tree leftChild;
 	private Tree rightChild;
-	
+
 	public Tree() {
 	}
+
 	public Tree(Integer data, Tree leftChild, Tree rightChild) {
 		this.data = data;
 		this.leftChild = leftChild;
 		this.rightChild = rightChild;
 	}
-	
+
 	public Tree(Integer data) {
 		this.data = data;
 	}
+
 	public Integer getData() {
 		return data;
 	}
+
 	public void setData(Integer data) {
 		this.data = data;
 	}
+
 	public Tree getLeftChild() {
 		return leftChild;
 	}
+
 	public void setLeftChild(Tree leftChild) {
 		this.leftChild = leftChild;
 	}
+
 	public Tree getRightChild() {
 		return rightChild;
 	}
+
 	public void setRightChild(Tree rightChild) {
 		this.rightChild = rightChild;
 	}
